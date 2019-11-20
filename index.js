abbfba91f43f417d23d270235c39d57f4aaf5b96
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-11-20 17:15:04 +0530

    setup partials and layputs

--- index.js
+++ index.js
@@ -2,13 +2,17 @@ const express = require('express');
 const port = 8000;
 const app = express();
 const path = require('path');
+const expressLayouts = require('express-ejs-layouts');
+
+// Setting the layout before router
+app.use(expressLayouts);
+
+// Use Express Router
+app.use('/', require('./routes'));
 
 // setup view engine
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
-
-// Use Express Router
-app.use('/', require('./routes'));
 
 app.listen(port, function(err){
     if(err) {
