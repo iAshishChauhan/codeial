cf5fec0002c1521ab7fbaae404f17d78835cbfeb
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-11-20 18:48:43 +0530

    setup MongoDB database connection

--- index.js
+++ index.js
@@ -3,9 +3,17 @@ const port = 8000;
 const app = express();
 const path = require('path');
 const expressLayouts = require('express-ejs-layouts');
+const db = require('./config/mongoose');
+
+// Setting up static files.
+app.use(express.static('./assets'));
 
 // Setting the layout before router
 app.use(expressLayouts);
+
+// extract styles and scripts from subpages into the layout.
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 // Use Express Router
 app.use('/', require('./routes'));
@@ -14,6 +22,7 @@ app.use('/', require('./routes'));
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+// Listen to Server on port no 8000
 app.listen(port, function(err){
     if(err) {
         console.log(`Error in running the server: ${err}`);
