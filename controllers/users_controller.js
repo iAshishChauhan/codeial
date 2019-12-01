16d041515ed55c88e42dceacc067f84992ddf56d
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-12-01 16:14:44 +0530

    Add Login/Signup Controller

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,7 +1,32 @@
 // Exporting the actions.
+// Actions
 
 module.exports.profile = function(req,res) {
     return res.render('user_profile', {
         title: "Profile"
     });
+}
+
+// render the signup page
+module.exports.UserSignUp = function(req, res) {
+    res.render('user_sign_up', {
+        title: "Codial | Sign Up"
+    });
+}
+
+// render the signin page
+module.exports.UserSignIn = function(req, res) {
+    res.render('user_sign_in', {
+        title: "Codial | Sign In"
+    });
+}
+
+// get the signup data
+module.exports.create = function(req, res) {
+    // TODO Later
+}
+
+// Sign In and create a Session
+module.exports.createSession = function(req, res) {
+    // TODO Later
 }
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const profileController = require('../controllers/users_controller');
-const postsController = require('../controllers/posts_controller');
 
 router.get('/profile', profileController.profile);
-router.get('/posts', postsController.posts);
+router.get('/sign-in', profileController.UserSignIn);
+router.get('/sign-up', profileController.UserSignUp);
 
 module.exports = router;
