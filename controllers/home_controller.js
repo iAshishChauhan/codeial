7cf582faf457934b464e36d9bcdae4d7e93569f7
iAshishChauhan <iashishsinghchauhan@gmail.com>
2019-12-10 02:59:06 +0530

    display posts of related user

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,16 +1,19 @@
 const Post = require('../models/post');
 
 module.exports.home = function (req, res) {
-    Post.find({}, function (err, post) {
+    // Populate the user of each post.
+
+    Post.find({}).populate('user').exec(function (err, posts) {
         if (err) {
             console.log('Error in getting Posts from DB');
             return;
         }
         return res.render('home', {
-            title: "Home",
-            user_post: post
+            title: "Codial | Home",
+            posts: posts
         });
     });
+
 }
 
 
